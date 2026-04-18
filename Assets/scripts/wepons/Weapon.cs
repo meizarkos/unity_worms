48a5d465516f5a49416a5ce4730550932312e173
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2026-04-19 00:06:03 +0200

    TOO LONG

--- Assets/scripts/wepons/Weapon.cs
+++ Assets/scripts/wepons/Weapon.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+    public float maxPower;
+    public float minAngle;
+    public float maxAngle;
+
+    public int dotCount = 5;
+
+    public float timeStep = 0.03f;
+    public float disableDots = 1.5f;
+
+    protected float smooth = 30f;
     public abstract void Fire();
 }
--- Assets/scripts/wepons/gun/GunBullet.cs
+++ Assets/scripts/wepons/gun/GunBullet.cs
@@ -5,24 +5,17 @@ public class GunBullet : MonoBehaviour
     public float speed = 20f;
     public float lifeTime = 3f;
     private Rigidbody2D rb;
-    private float rotation = 90f;
+    private readonly float rotation = 90f;
 
     public int damage = 5;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = transform.right * speed + transform.up * 1;
+        rb.gravityScale = 0;
+        rb.linearVelocity = transform.right * speed;
+        rb.rotation = transform.eulerAngles.z - rotation;
         Destroy(gameObject,lifeTime);
-    }
-
-    void FixedUpdate()
-    {
-        if (rb.linearVelocity.sqrMagnitude > 0.01f)
-        {
-            float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
-            rb.rotation = angle + rotation;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
